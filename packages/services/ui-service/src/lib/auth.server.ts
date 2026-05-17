7c823d5c236532c5b7435b54f3482fe1778cff4a
Kevin Zheng <kevin.zheng@monash.edu>
2026-05-18 04:01:45 +1000

    Minor fix

--- packages/services/ui-service/src/lib/auth.server.ts
+++ packages/services/ui-service/src/lib/auth.server.ts
@@ -1,21 +1,17 @@
-import { createServerFn } from "@tanstack/react-start";
+import { createServerOnlyFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
 import { auth } from "src/lib/get-auth";
 
-export const getAuthSession = createServerFn({ method: "GET" }).handler(
-  async () => {
-    const headers = getRequestHeaders();
-    return await (await auth()).api.getSession({ headers });
-  },
-);
+export const getAuthSession = createServerOnlyFn(async () => {
+  const headers = getRequestHeaders();
+  return await (await auth()).api.getSession({ headers });
+});
 
-export const ensureAuthSession = createServerFn({ method: "GET" }).handler(
-  async () => {
-    const headers = getRequestHeaders();
-    const session = await (await auth()).api.getSession({ headers });
-    if (!session) {
-      throw new Error("Unauthorized");
-    }
-    return session;
-  },
-);
+export const ensureAuthSession = createServerOnlyFn(async () => {
+  const headers = getRequestHeaders();
+  const session = await (await auth()).api.getSession({ headers });
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+  return session;
+});
