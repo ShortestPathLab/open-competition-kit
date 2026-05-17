5138833657e1d920245dcdfc26f50cee6dbb148c
Kevin Zheng <kevin.zheng@monash.edu>
2026-05-18 01:26:20 +1000

    Add GitHub Actions workflow for publishing packages and create publish script

---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+set -eu
+
+directories="
+packages/core
+packages/packages/db/prisma
+packages/packages/form/json
+packages/packages/integration/github-classic
+packages/packages/leaderboard/ag-grid
+packages/packages/noop
+packages/packages/standard
+packages/sdk
+"
+
+for dir in $directories; do
+  echo "Publishing $dir..."
+
+  cd "$dir"
+
+  bun publish --access public
+
+  cd - >/dev/null
+done
